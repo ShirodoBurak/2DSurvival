968f3981b982bfa0a5d4d7cdf82b3b8412adeaae
BurakKaya <64273202+ShirodoBurak@users.noreply.github.com>
2020-10-06 15:52:38 +0300

    First commit
    First commit

---
+++ Assets/GenerateTilemapWorld.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Unity.Jobs;
+
+public class GenerateTilemapWorld : MonoBehaviour {
+    [Header("Surface Blocks")]
+    public TileBase grass;
+    public TileBase dirt;
+    [Header("Underground Blocks")]
+    public TileBase stone;
+    [Header("World Settings")]
+    public int chunkWidth;
+    [Space(10)]
+    public int mapSizeX;
+    public int mapSizeY;
+    [Space(10)]
+    public int X;
+    public int Y;
+    public int Seed;
+    public int Octaves;
+    public float Scale, Persistence, Lacunarity, additionalLacunarity, smoothness;
+    int lastX;
+    float[,] noiseMap;
+    float[,] noiseMap2;
+    float[,] noiseMapSum;
+    int AOEOP20_EatHROLA2 = 0;
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            AOEOP20_EatHROLA2 = 0;
+            noiseMap = Noise.GenerateNoiseMap(mapSizeX, mapSizeY, Seed, Scale, Octaves, Persistence, Lacunarity, new Vector2(X, Y));
+            noiseMap2 =Noise.GenerateNoiseMap(mapSizeX, mapSizeY, Seed, Scale, Octaves, Persistence, Lacunarity+additionalLacunarity, new Vector2(X, Y));
+            int index=0;
+            foreach (var item in noiseMap)
+            {
+                noiseMap[0, index] = noiseMap[0, index] + (noiseMap2[0, index] / smoothness);
+                index++;
+            }
+            index = 0;
+            ThreadWorldGeneration(new Job());
+            /*
+            foreach (float item in noiseMapSum)
+            {
+                int i = Mathf.FloorToInt(item * 100);   
+                this.gameObject.GetComponent<Tilemap>().SetTile(new Vector3Int(AOEOP20_EatHROLA2++, i, 0), dirt);
+            }
+            */
+        }
+    }
+
+    void ThreadWorldGeneration(Job job)
+    {
+        TileBase[] tiles = { dirt, grass, stone };
+        job.lastX = lastX;
+        lastX=lastX + 16;
+        job.noiseMap = noiseMap;
+        job.tilemap = this.gameObject.GetComponent<Tilemap>();
+        job.AOEOP20_EatHROLA2 = AOEOP20_EatHROLA2;
+        job.blockList = tiles;
+        job.chunkWidth = chunkWidth;
+        job.Execute();
+    }
+    IEnumerator OptimizedWorldGeneration() {
+        int currentLastX = lastX;
+        lastX = lastX + 16;
+        for (int j = 0; j<chunkWidth; j++) {
+            int h = Mathf.FloorToInt(noiseMap[0,currentLastX+j] * 100);
+            this.gameObject.GetComponent<Tilemap>().SetTile(new Vector3Int(AOEOP20_EatHROLA2++, j, 0), dirt);
+            for (int i = 0; i<h; i++) {
+                int randomDirtHeight = Random.Range(15, 19);
+                if(i == h-1) {
+                    this.gameObject.GetComponent<Tilemap>().SetTile(new Vector3Int(j+currentLastX, i, 0), grass);
+                }
+                else if(i < h && i >h-randomDirtHeight) {
+                    this.gameObject.GetComponent<Tilemap>().SetTile(new Vector3Int(j+currentLastX, i, 0), dirt);
+                }
+                else {
+                    this.gameObject.GetComponent<Tilemap>().SetTile(new Vector3Int(j+currentLastX, i, 0), stone);
+                }
+
+                
+            }
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+}
---
+++ Assets/Scripts/followCursor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCursor : MonoBehaviour {
+
+    public Vector3 MousePos;
+    public RaycastHit2D rayHit;
+    public RaycastHit2D hit;
+   
+    void Start() {
+        Cursor.visible=false;
+        StartCoroutine(BreakBlock());
+    }
+    void FixedUpdate() {
+        hit=Physics2D.Raycast(origin: MousePos, direction: Vector2.zero);
+        MousePos=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       
+        this.transform.position=new Vector3(MousePos.x, MousePos.y, 0);
+    }
+    IEnumerator BreakBlock() {
+        while (true)
+        {
+            if(Input.GetMouseButtonDown(0)) {
+                if(hit.collider!=null&&hit.collider.gameObject.name!="Player") {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+            yield return new WaitForSeconds(.001f);
+        }
+    }
+}
